7e475ce54e642bbf6b80bb580609b673f1374854
Ali <65233907+Alisakhanli@users.noreply.github.com>
2021-10-21 19:01:12 +0400

    Add files via upload

---
+++ homework/script.js
@@ -0,0 +1,72 @@
+// Task 1
+// function GetSequence(a,b) {
+//     let final = []
+//     for (let i = a; i <= b; i++) {
+//         final.push(i)
+
+//     }
+//     return final;
+// }
+
+//Task 2
+
+// function Sum(a,b) {
+//     if (a==b) {
+//         return a*3;
+
+//     }
+//     else {
+//         return a+b;
+//     }
+
+// }
+
+//Task 3
+
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// function PrintAll() {
+//   for (let i = 0; i < array.length; i++) {
+//     console.log(array[i]);
+//   }
+// }
+
+// function Add() {
+//     array.push(15)
+    
+// }
+
+// function Delete() {
+//     array.shift()
+    
+// }
+
+// function SortAndPrint() {
+//     array.sort((a,b)=> a-b)
+//     console.log(array[array.length-1])
+    
+// }
+
+// function After3() {
+//     for (let i = 3; i < array.length; i++) {
+//         console.log(array[i])
+        
+//     }
+    
+// }
+
+// function ForEach() {
+   
+//     array.forEach(item=>console.log(item))
+    
+// }
+// function Print40() {
+//     for (let i = 0; i < array.length; i++) {
+//       if (array[i]>40) {
+//           console.log(console[i])
+          
+//       }
+//       else continue;
+        
+//     }
+    
+// }
